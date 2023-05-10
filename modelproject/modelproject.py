d75b070f11fc6b088fc25a8751a2df91428c05f4
JulieVoss <rqx424@alumni.ku.dk>
2023-05-10 17:28:11 +0200

    Modelproject fejler

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,6 +1,7 @@
-from scipy import optimize
 from types import SimpleNamespace
+import time
 import numpy as np
+from scipy import optimize
 
 class modelclass():
 
@@ -9,10 +10,6 @@ class modelclass():
 
         self.par = SimpleNamespace()
         self.sim = SimpleNamespace()
-
-
-    def setup(self):
-        """ create and setup the model """
 
         par = self.par 
 
@@ -26,6 +23,18 @@ class modelclass():
         # Length of simulations
         par.simT = 10_000
 
+    def allocate(self):
+        """ allocate arrays for simulation """
+        par = self.par
+        sim = self.sim
+
+        household = ['C1','C2']
+        prices = ['w', 'r', 'rt']
+        firm = ['K']
+
+        allvarnames = household + firm + prices 
+        for varname in allvarnames:
+            sim.__dict__[varname] = np.nan*np.ones(par.simT)
 
     def simulate(self):
         """ simulate the full model """
@@ -33,28 +42,32 @@ class modelclass():
         par = self.par 
         sim = self.sim
 
+        self.__init__()
+        self.allocate()
+
         # values
-        sim.k[0] = par.k
+        sim.k = par.k
 
         for t in range(par.simT):
             # i. simulate before s
-            simulate_before_s(par,sim,t)
+        
+            self.simulate_before_s(par,sim,t,s)
 
-            if t == par.simT-1: continue          
+            if t == par.simT: continue          
 
             # ii. find optimal s
-            obj = lambda s: calc_euler_error(s,par,sim,t=t)
+            obj = lambda s: self.calc_euler_error(par,sim,t=t)
             result = optimize.root_scalar(obj,bracket=[1E-10,10],method='bisect')
             s = result.root
 
             # iii. simulate after s
-            simulate_after_s(par,sim,t,s)
+            self. simulate_after_s(par,sim,t,s)
 
 
-    def simulate_before_s(par,sim,t,s):
+    def simulate_before_s(self,par,sim,t,s):
         """ simulate forward"""
 
-        if t > 0:
+        if t >= 0:
             sim.k[t] = sim.k[t+1]
         else: 
             raise NotImplementedError('')
@@ -62,13 +75,15 @@ class modelclass():
         sim.C2[t] = s*(1+sim.r) + par.tau * sim.w
 
 
-    def simulate_after_s(par,sim,t,s):
+    def simulate_after_s(self, par,sim,t,s):
         sim.C1[t] = sim.w[t]*(1-par.tau) - s
 
 
-    def calc_euler_error(s,par,sim,t):
-        simulate_after_s(par,sim,t,s)
-        simulate_before_s(par,sim,t+1)
+    def calc_euler_error(self,s,par,sim,t):
+
+
+        self.simulate_after_s(par,sim,t,s)
+        self.simulate_before_s(par,sim,t+1)
 
         LHS = sim.C2[t+1]
         RHS = (1+sim.r[t+1])/(1+par.rho) * sim.C1
