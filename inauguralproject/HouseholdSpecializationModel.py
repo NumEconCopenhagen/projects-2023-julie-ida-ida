8a6bda48967b6b0dcccad353f7ddb783d6d2d377
JulieVoss <rqx424@alumni.ku.dk>
2023-05-23 10:38:28 +0200

    Simulering + opg. 5

--- inauguralproject/HouseholdSpecializationModel.py
+++ inauguralproject/HouseholdSpecializationModel.py
@@ -13,30 +13,30 @@ class HouseholdSpecializationModelClass:
     def __init__(self):
         """ setup model """
 
-        # a. create namespaces
+        # a. Create namespaces
         par = self.par = SimpleNamespace()
         sol = self.sol = SimpleNamespace()
 
-        # b. preferences
-        par.rho = 2.0
+        # b. Preferences
+        par.rho = 2.0 
         par.nu = 0.001
         par.epsilon = 1.0
         par.omega = 0.5 
 
-        # c. household production
+        # c. Household production
         par.alpha = 0.5
         par.sigma = 1.0
 
-        # d. wages
+        # d. Wages
         par.wM = 1.0
         par.wF = 1.0
         par.wF_vec = np.linspace(0.8,1.2,5)
 
-        # e. targets
+        # e. Targets 
         par.beta0_target = 0.4
         par.beta1_target = -0.1
 
-        # f. solution
+        # f. Solution
         sol.LM_vec = np.zeros(par.wF_vec.size)
         sol.HM_vec = np.zeros(par.wF_vec.size)
         sol.LF_vec = np.zeros(par.wF_vec.size)
@@ -51,10 +51,10 @@ class HouseholdSpecializationModelClass:
         par = self.par
         sol = self.sol
 
-        # a. consumption of market goods
+        # a. Consumption of market goods
         C = par.wM*LM + par.wF*LF
 
-        # b. home production
+        # b. Consumption of home production
         if par.sigma == 0:
             H = np.min(HM,HF)
         elif par.sigma == 1:
@@ -62,11 +62,11 @@ class HouseholdSpecializationModelClass:
         else:
             H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**(par.sigma/(par.sigma-1))
 
-        # c. total consumption utility
+        # c. Total consumption utility
         Q = C**par.omega*H**(1-par.omega)
-        utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
+        utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho) #OBS! Hvorfor skriver man Q,1e-8 her? 
 
-        # d. disutlity of work
+        # d. Disutlity of work
         epsilon_ = 1+1/par.epsilon
         TM = LM+HM
         TF = LF+HF
@@ -81,7 +81,7 @@ class HouseholdSpecializationModelClass:
         sol = self.sol
         opt = SimpleNamespace()
         
-        # a. all possible choices
+        # a. All possible choices
         x = np.linspace(0,24,49)
         LM,HM,LF,HF = np.meshgrid(x,x,x,x) # all combinations
     
@@ -90,14 +90,14 @@ class HouseholdSpecializationModelClass:
         LF = LF.ravel()
         HF = HF.ravel()
 
-        # b. calculate utility
+        # b. Calculate utility
         u = self.calc_utility(LM,HM,LF,HF)
     
-        # c. set to minus infinity if constraint is broken
-        I = (LM+HM > 24) | (LF+HF > 24) # | is "or"
+        # c. Set to minus infinity if constraint is broken
+        I = (LM+HM > 24) | (LF+HF > 24) 
         u[I] = -np.inf
     
-        # d. find maximizing argument
+        # d. Find maximizing argument
         j = np.argmax(u)
         
         opt.LM = LM[j]
@@ -105,7 +105,7 @@ class HouseholdSpecializationModelClass:
         opt.LF = LF[j]
         opt.HF = HF[j]
 
-        # e. print
+        # e. Print
         if do_print:
             for k,v in opt.__dict__.items():
                 print(f'{k} = {v:6.4f}')
@@ -117,21 +117,18 @@ class HouseholdSpecializationModelClass:
         sol = self.sol
         par = self.par
 
+        # a. Creating an empty list to store the values
         sol.HM_wage_vec = []
         sol.HF_wage_vec = []
         wF = (0.8, 0.9, 1.0, 1.1, 1.2)
 
 
-        # b. for loop
+        # b. For loop
         for wages in wF:
             par.wF = wages
             _, _, HM, HF = self.solve_con()
             sol.HM_wage_vec.append(HM)
             sol.HF_wage_vec.append(HF)
-            
-        #c. extracting results
-        #HF_wage_vec = [ns[3] for ns in sol.solution_wage]
-        #HM_wage_vec = [ns[2] for ns in sol.solution_wage]
 
         H_ratio = [np.log(HF_ny/HM_ny) for HF_ny, HM_ny in zip(sol.HF_wage_vec, sol.HM_wage_vec)]
         w_ratio = np.log(wF)  
@@ -161,7 +158,6 @@ class HouseholdSpecializationModelClass:
                                    constraints=constraints,
                                    tol=1e-10)
 
-            
         LM = solution.x[0]
         HM = solution.x[1]
         LF = solution.x[2]
@@ -184,7 +180,6 @@ class HouseholdSpecializationModelClass:
 
             bounds = [(1e-8,24-1e-8)]*4
             guess = [2*12/2]*4
-            
 
             solution4 = optimize.minimize(objective4,
                                    guess,
@@ -199,19 +194,17 @@ class HouseholdSpecializationModelClass:
             sol.HF_vec[i] = solution4.x[3]
 
         return sol.HM_vec, sol.HF_vec
-
-
+    
     def run_regression(self):
         """ run regression """
 
         par = self.par
         sol = self.sol
 
-        x = np.log(par.wF_vec)
+        x = np.log(par.wF_vec) # Er det den rigtige wF_vec der bruges her? Tænker det skal være for wF in [0.8,0.9,1.0,1.1,1.2] og lige nu tror jeg det er (0.8,1.2,5.0)
         y = np.log(sol.HF_vec/sol.HM_vec)
         A = np.vstack([np.ones(x.size),x]).T
         sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0]
-    
 
     def loss(self, x):
         """ estimate alpha and sigma """
@@ -251,3 +244,135 @@ class HouseholdSpecializationModelClass:
         print(f' optimal sigma = {opt_as.sigma4}')
 
         return solution4a
+
+    # QUESTION 5 - alt nedenfor her er kopieret fra jos-p-og-tex og skal ændres i/slettes
+    def extended_model(self,LM,HM,LF,HF):
+        """ calculate utility """
+
+        par = self.par
+        sol = self.sol
+
+        # a. consumption of market goods
+        C = par.wM*LM + par.wF*LF
+
+        # b. home production
+        if par.sigma == 1:
+            H = HM**(1-par.alpha)*HF**par.alpha
+        elif par.sigma == 0:
+            H = min(HM,HF)
+        else:
+            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma) + par.alpha*HF**((par.sigma-1)/par.sigma))**(par.sigma/(par.sigma-1)) 
+        
+        # c. total consumption utility
+        Q = C**par.omega*H**(1-par.omega)
+        utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
+
+        # d. disutility of work
+        epsilon_ = 1+1/par.epsilon
+        TM = LM+HM
+        TF = LF+HF
+        disutility = par.nu*(TM**epsilon_/epsilon_+TF**epsilon_/epsilon_)
+
+        # e. preferences for housework
+        pref_H_work = par.k*(HF/HM - HM/HF)
+        
+        return utility - disutility + pref_H_work
+    
+    # Solving the model as before, but know using the extended version
+    def solve_extension(self):
+        """ solve model continously """
+
+        par = self.par 
+        opt = SimpleNamespace()  
+
+        # a. objective function (to minimize) - including penalty to account for time constraints (for Nelder-Mead method)
+        def obj(x):
+            LM,HM,LF,HF=x
+            penalty=0
+            time_M = LM+HM
+            time_F = LF+HF
+            if time_M > 24 or time_F > 24:
+                penalty += 1000 * (max(time_M, time_F) - 24)
+            return -self.extended_model(LM,HM,LF,HF) + penalty
+
+        # b. call solve
+        x0=[2,2,2,2] # initial guess
+        result = optimize.minimize(obj,x0,method='Nelder-Mead')
+        
+        # d. save results
+        opt.LM = result.x[0]
+        opt.HM = result.x[1]
+        opt.LF = result.x[2]
+        opt.HF = result.x[3]
+        
+        return opt
+
+    # Solving the model for a vector of female wages
+    def solve_wF_vec5(self,discrete=False):
+        """ solve model for vector of female wages """
+
+        par = self.par
+        sol = self.sol
+    
+        for i,wF in enumerate(par.wF_vec):
+            par.wF = wF
+            
+            # Running the model and replacing the values in the vectors with the optimal values
+            opt = self.solve_extension()
+            sol.HF_vec[i]=opt.HF
+            sol.HM_vec[i]=opt.HM
+
+        return sol.HF_vec, sol.HM_vec
+    
+    # Defining a method to run the regression
+    def run_regression5(self):
+        """ run regression """
+
+        par = self.par
+        sol = self.sol
+
+        self.solve_wF_vec5()
+
+        x = np.log(par.wF_vec)
+        y = np.log(sol.HF_vec/sol.HM_vec)
+        A = np.vstack([np.ones(x.size),x]).T
+        sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0] 
+
+        return sol.beta0,sol.beta1
+
+    # Defining a method to find the optimal value of k to match the target values for beta0 and beta1
+    def optimal_k(self):
+        """ find alpha and sigma to match data """
+
+        par = self.par 
+        sol = self.sol
+
+        def objective_new(k):
+            par.k = k
+
+            # Assign the parameter values and define the target values for beta0 and beta1
+            beta0 = 0.4
+            beta1 = -0.1
+
+            # Run the regression for the different vector of ratios between home production and wages when the parameters vary
+            sol.beta0,sol.beta1 = self.run_regression5()
+
+            # Compute the objective value
+            val = (beta0 - sol.beta0)**2 + (beta1 - sol.beta1)**2
+
+            return val
+
+        # Initial guess for k
+        initial_guess_k = -0.00002
+
+        # Optimization
+        result = optimize.minimize(objective_new, initial_guess_k, method='Nelder-Mead')
+
+        # Extract the optimized values
+        sol.optimal_k = result.x[0]
+
+        # Printing the results
+        print(f"Minimum value: {result.fun:.2f}")
+        print(f"Optimal k: {sol.optimal_k:.5f}")
+        print(f"Beta0: {sol.beta0:.2f}")
+        print(f"Beta1: {sol.beta1:.2f}")
--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -4,6 +4,8 @@ import time
 import numpy as np
 from scipy import optimize
 
+import matplotlib.pyplot as plt
+
 class modelclass():
 
     def __init__(self,do_print=True):
@@ -25,19 +27,19 @@ class modelclass():
         par = self.par
 
         # a. Household parameters 
-        par.rho = 0.03 
+        par.rho = 0.03 # discount rate 
         par.n = 0.05 # population growth 
 
         # b. Firms parameters 
-        par.p_f = 'cobb-douglas' # OBS! Hvorfor skriver man denne ind? 
-        par.alpha = 0.3
+        par.p_f = 'cobb-douglas' # production function 
+        par.alpha = 0.3 # percentage of capital the consumer uses in their production process 
 
         # c. Government parameters 
-        par.tau = 0.0 # OBS! Wage tax? Troede det var bidraget fra de unge til de gamle i samme periode? 
+        par.tau = 0.0 # wage tax
 
         # d. Start values and length of simulation
         par.K_ini = 0.1 # initial capital stock 
-        par.L_ini = 1.0 # OBS! Hvad er dette? De skriver initial population 
+        par.L_ini = 1.0 # initial population 
         par.simT = 20 # length og simulation 
 
     def allocate(self):
@@ -76,7 +78,7 @@ class modelclass():
             self.simulate_before_s(par,sim,t)
             if t == par.simT-1: continue 
 
-            # ii. Find bracket to search 
+            # ii. Find bracket to search in
             s_min,s_max = self.find_s_bracket(par,sim,t)
 
             # iii. Find optimal s 
@@ -91,7 +93,7 @@ class modelclass():
             # v. Simulate after s 
             self.simulate_after_s(par,sim,t,s)
         
-        if do_print: print(f'simulation done in {time.time()-t0:.2f} secs')
+        if do_print: print(f'simulation done in {time.time()-t0:.3f} secs')
 
     def find_s_bracket(self,par,sim,t,maxiter=500,do_print=False):
         """ find bracket for s to search in """
@@ -103,7 +105,7 @@ class modelclass():
         # b. It is always possible to save a lot 
         value = self.calc_euler_error(s_max,par,sim,t)
         sign_max = np.sign(value)
-        if do_print: print(f'euler-error for s = {s_max:12.8f} = {value:12.8f}')
+        if do_print: print(f'euler-error for s = {s_max:12.3f} = {value:12.3f}')
 
         # c. Find brackets 
         lower = s_min 
@@ -116,7 +118,7 @@ class modelclass():
             s = (lower+upper)/2 # midpoint
             value = self.calc_euler_error(s,par,sim,t)
 
-            if do_print: print(f'euler-error for s = {s:12.8f} = {value:12.8f}')
+            if do_print: print(f'euler-error for s = {s:12.3f} = {value:12.3f}')
 
             # ii. Check conditions
             valid = not np.isnan(value)
@@ -128,7 +130,7 @@ class modelclass():
                 s_max = upper
                 if do_print: 
                     print(f'bracket to search in with opposite signed errors:')
-                    print(f'[{s_min:12.8f}-{s_max:12.8f}]')
+                    print(f'[{s_min:12.3f}-{s_max:12.3f}]')
                 return s_min,s_max
             elif not valid: # too low s -> increase lower bound
                 lower = s
@@ -169,22 +171,23 @@ class modelclass():
         sim.r[t] = par.alpha * sim.K[t]**(par.alpha-1) * (sim.L[t])**(1-par.alpha)
         sim.w[t] = (1-par.alpha) * sim.K[t]**(par.alpha) * (sim.L[t])**(-par.alpha)
 
-        # c. Consumption 
-        sim.C2[t] = (1+sim.r[t])*(sim.K[t]) # Hvorfor K og ikke s?
+        # c. Consumption before s 
+        sim.C2[t] = (1+sim.r[t])*(sim.K[t]) 
     
     def simulate_after_s(self,par,sim,t,s):
         """ simulate forward """
 
-        sim.k[t] = sim.K[t]/sim.L[t]
+        sim.k[t] = sim.K[t]/sim.L[t] # capital per capita 
 
         # a. Consumption of young 
         sim.C1[t] = (1-par.tau)*sim.w[t]*(1.0-s) * sim.L[t] # Skal denne ændres?
 
         # b. End-of-periods stock 
         I = sim.Y[t] - sim.C1[t] - sim.C2[t]
-        sim.K[t+1] = sim.K[t]+I
+        sim.K[t+1] = sim.K[t]+I # Skal det være store eller lille k her??
 
     def sim_results(self):
+
         # a. Importing the model 
         model = modelclass()
 
@@ -196,32 +199,32 @@ class modelclass():
 
         par.beta = 1/(1+par.rho)
 
-        #sim.K[0] = par.K_ini
-        #sim.L[0] = par.L_ini
+        sim.K[0] = par.K_ini
+        sim.L[0] = par.L_ini
 
         # c. Simulating t = 0 and t 
         self.simulate_before_s(par,sim,t=0)
-        print('Consumption by old people in period t = 0',f'{sim.C2[0] = : .4f}')
+        print('Consumption by old people in period t = 0',f'{sim.C2[0] = : .3f}')
 
         self.simulate_after_s(par,sim,s=s_guess,t=0)
-        print('Consumption by young in period t = 0',f'{sim.C1[0] = : .4f}')
+        print('Consumption by young in period t = 0',f'{sim.C1[0] = : .3f}')
 
         self.simulate_before_s(par,sim,t=1)
-        print('Consumption by old people in period t',f'{sim.C2[1] = : .4f}')
+        print('Consumption by old people in period t',f'{sim.C2[1] = : .3f}')
 
         self.simulate_after_s(par,sim,s=s_guess,t=1)
-        print('Consumption by young people in period t',f'{sim.C1[1] = : .4f}')
+        print('Consumption by young people in period t',f'{sim.C1[1] = : .3f}')
 
         # d. Calculating the Euler error
         LHS_Euler = sim.C1[0]**(-1)
-        RHS_Euler = (1 + sim.r[1])*par.beta * sim.C2[1]**(-1)
-        print(f'euler-error = {LHS_Euler-RHS_Euler:.8f}')
+        RHS_Euler = (1+sim.r[1])*par.beta * sim.C2[1]**(-1)
+        print(f'euler-error = {LHS_Euler-RHS_Euler:.3f}')
 
-        # e. Check if Euler error converges towards 0:
+        # e. Check if the Euler error goes towards 0
         model.simulate()
         LHS_Euler = sim.C1[18]**(-1)
         RHS_Euler = (1+sim.r[19])*par.beta * sim.C2[19]**(-1)
         print("euler error after model has been simulated", LHS_Euler-RHS_Euler)
 
-        # d. Save steady state for this version
+        # e. Save steady state for this version
         sim.k_orig=sim.k.copy()
